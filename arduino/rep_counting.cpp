74a9ff0436768d2d9317bbce67001fd9ad07627c
Florian Mealing <florianmealing@gmail.com>
2026-03-08 13:31:35 +0000

    [Feat] Added the Low Pass Filter

--- arduino/rep_counting.cpp
+++ arduino/rep_counting.cpp
@@ -1,3 +1,10 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+// Constants
+const int cutoffFrequency = 10; // Hz
+
+// Custum Types
 struct Vector3 {
     float x, y, z;
 };
@@ -7,3 +14,50 @@ struct ImuData {
     Vector3 gyro; // rad/s
     unsigned long time_ms; // millis()
 };
+
+struct LowPassFilter {
+    ImuData last_output;
+    bool initialised;
+};
+
+
+/*
+ * Function that applies a low pass filter to data from an IMU
+ * Inputs: current IMU data, the previous LowPassFilter result
+ * Output: value of sensor with low-pass-filter
+ * 
+ * Equation: filtered[n] = alpha  * x[n] + (1 - alpha) * filtered[n-1]
+ * where
+ * - x[n] = current raw sample 
+ * - filtered[n-1] = previous filtered output
+ * - alpha = dt / (dt + RC)
+ * - RC = 1 / (2 * pi * cuttoffFrequency)
+*/
+ImuData applyLowPassFilter(ImuData current, LowPassFilter &filter) {
+    // If the filter hasn't been initialised, just return the current data
+    if (!filter.initialised) {
+        filter.last_output = current;
+        filter.initialised = true;
+        return current;
+    }
+
+    // Caluclating the alpha value
+    float dt = (current.time_ms - filter.last_output.time_ms) / 1000.0f;
+    float RC = 1.0f / (2.0f * M_PI * cutoffFrequency);
+    float alpha = dt / (dt + RC);
+
+    // Initialise the filtered ImuData value and populate with the filtered data (see equation)
+    ImuData filtered;
+    filtered.time_ms = current.time_ms;
+
+    filtered.acc.x = alpha * current.acc.x + (1 - alpha) * filter.last_output.acc.x;
+    filtered.acc.y = alpha * current.acc.y + (1 - alpha) * filter.last_output.acc.y;
+    filtered.acc.z = alpha * current.acc.z + (1 - alpha) * filter.last_output.acc.z;
+
+    filtered.gyro.x = alpha * current.gyro.x + (1 - alpha) * filter.last_output.gyro.x;
+    filtered.gyro.y = alpha * current.gyro.y + (1 - alpha) * filter.last_output.gyro.y;
+    filtered.gyro.z = alpha * current.gyro.z + (1 - alpha) * filter.last_output.gyro.z;
+
+    filter.last_output = filtered;
+    return filtered;
+}
